edcc932b6db2e8a88004622e64319775e87e2b28
Vybornak2 <jan.vyborny2@gmail.com>
2026-03-19 21:37:46 +0000

    nvim config split into multiple files

---
+++ nvim/lua/plugins/ui.lua
@@ -0,0 +1,50 @@
+return {
+	-- See `:help gitsigns` to understand what the configuration keys do
+	{ -- Adds git related signs to the gutter, as well as utilities for managing changes
+		"lewis6991/gitsigns.nvim",
+		---@module 'gitsigns'
+		---@type Gitsigns.Config
+		---@diagnostic disable-next-line: missing-fields
+		opts = {
+			signs = {
+				add = { text = "+" }, ---@diagnostic disable-line: missing-fields
+				change = { text = "~" }, ---@diagnostic disable-line: missing-fields
+				delete = { text = "_" }, ---@diagnostic disable-line: missing-fields
+				topdelete = { text = "‾" }, ---@diagnostic disable-line: missing-fields
+				changedelete = { text = "~" }, ---@diagnostic disable-line: missing-fields
+			},
+		},
+	},
+	{ -- You can easily change to a different colorscheme.
+		-- Change the name of the colorscheme plugin below, and then
+		-- change the command in the config to whatever the name of that colorscheme is.
+		--
+		-- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+		"folke/tokyonight.nvim",
+		priority = 1000, -- Make sure to load this before all the other start plugins.
+		config = function()
+			---@diagnostic disable-next-line: missing-fields
+			require("tokyonight").setup({
+				styles = {
+					comments = { italic = false }, -- Disable italics in comments
+				},
+			})
+
+			-- Load the colorscheme here.
+			-- Like many other themes, this one has different styles, and you could load
+			-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+			vim.cmd.colorscheme("tokyonight-night")
+		end,
+	},
+
+	-- Highlight todo, notes, etc in comments
+	{
+		"folke/todo-comments.nvim",
+		event = "VimEnter",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		---@module 'todo-comments'
+		---@type TodoOptions
+		---@diagnostic disable-next-line: missing-fields
+		opts = { signs = false },
+	},
+}
