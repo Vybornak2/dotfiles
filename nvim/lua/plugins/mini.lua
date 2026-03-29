ed8433fe02c6bde87ecca180bc8663e2b6786c21
Vybornak2 <jan.vyborny2@gmail.com>
2026-03-29 15:44:32 +0200

    Nvim config changes: - buffer unlisting split it responsible packages - added `mini.bufremove`

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -7,11 +7,21 @@ return {
 		local statusline = require("mini.statusline")
 		statusline.setup({ use_icons = vim.g.have_nerd_font })
 
+		require("mini.bufremove").setup()
 		require("mini.files").setup({
 			options = {
 				use_as_default_explorer = true,
 				permanent_delete = true,
 			},
+		})
+
+		-- Autocmd: Unlist minifiles buffers
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = { "minifiles" },
+			callback = function(args)
+				vim.bo[args.buf].buflisted = false
+				vim.bo[args.buf].bufhidden = "wipe"
+			end,
 		})
 	end,
 }
--- nvim/lua/plugins/neogit.lua
+++ nvim/lua/plugins/neogit.lua
@@ -10,4 +10,18 @@ return {
 	keys = {
 		{ "<leader>gg", "<cmd>Neogit<cr>", desc = "[G]it" },
 	},
+	config = function()
+		-- Autocmd: Unlist Neogit plugin buffers
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = {
+				"NeogitStatus",
+				"NeogitPopup",
+				"NeogitCommitMessage",
+			},
+			callback = function(args)
+				vim.bo[args.buf].buflisted = false
+				vim.bo[args.buf].bufhidden = "wipe"
+			end,
+		})
+	end,
 }
