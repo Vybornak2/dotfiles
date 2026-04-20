0e93ee6ea8a4077a9d348a934bc1bdee56ead99f
Vybornak2 <jan.vyborny2@gmail.com>
2026-04-21 00:44:11 +0200

    Nvim config changes: - noice clean-up

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -6,19 +6,18 @@ return {
             override = {
                 ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
                 ["vim.lsp.util.stylize_markdown"] = true,
-                ["cmp.entry.get_documentation"] = true, 
+                ["cmp.entry.get_documentation"] = true,
             },
         },
         presets = {
-            bottom_search = false,    -- Changed to false: Now `/` search will be a floating popup!
-            command_palette = true,   -- Keeps the `:` cmdline looking like a modern palette
+            bottom_search = false,
+            command_palette = true,
             long_message_to_split = true,
             inc_rename = false,
-            lsp_doc_border = true,    -- Changed to true: Gives your `K` hover window rounded borders
+            lsp_doc_border = true,
         },
     },
     dependencies = {
         "MunifTanjim/nui.nvim",
-        "rcarriga/nvim-notify",
     }
 }
