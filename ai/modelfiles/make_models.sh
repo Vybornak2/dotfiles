5b2a5721c14e62425e72d0065e3f68da7a0cdb8f
Vybornak2 <jan.vyborny2@gmail.com>
2026-07-01 16:26:48 +0200

    Fix ollama modelfile, added `make_models.sh` script

---
+++ ai/modelfiles/make_models.sh
@@ -0,0 +1,2 @@
+ollama create qwen2.5-coder:7b-caveman -f "/home/vybornak/dotfiles/ai/modelfiles/qwen2.5-coder-7b-caveman"
+ollama create gemma4:e4b-caveman -f "/home/vybornak/dotfiles/ai/modelfiles/gemma4-e4b-caveman"
