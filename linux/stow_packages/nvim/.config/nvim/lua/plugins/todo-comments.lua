6fb87433f15b677cc59bf00aae57073ea6cab020
Vybornak2 <jan.vyborny2@gmail.com>
2026-08-04 09:13:48 +0200

    Updated color scheme for `todo-comments`

--- linux/stow_packages/nvim/.config/nvim/lua/plugins/todo-comments.lua
+++ linux/stow_packages/nvim/.config/nvim/lua/plugins/todo-comments.lua
@@ -20,36 +20,26 @@ return {
     opts = {
         signs = true,
         keywords = {
-            NOTE = { icon = "", color = "note", alt = { "note" } },
-            WARN = { icon = " ", color = "warn", alt = { "warn" } },
+            FIX = { icon = " ", color = "error", alt = { "fix" } },
 
-            TODO = { icon = " ", color = "todo", alt = { "todo" } },
-            IMPL = { icon = "", color = "impl", alt = { "impl" } }, -- nf-fa-wrench
-            FIX = { icon = " ", color = "fix", alt = { "fix" } },
-            REFACTOR = { icon = "", color = "refactor", alt = { "refactor" } }, -- nf-fa-gear
+            WARN = { icon = " ", color = "warning", alt = { "warn" } },
+            IMPL = { icon = "", color = "warning", alt = { "impl" } },
+            REFACTOR = { icon = "", color = "warning", alt = { "refactor" } }, -- nf-fa-gear
 
-            REVIEW = { icon = " ", color = "review", alt = { "review" } }, -- nf-fa-pencil
-            PERF = { icon = "", color = "perf", alt = { "perf" } },
-            TEST = { icon = "", color = "test", alt = { "test" } },
+            NOTE = { icon = "", color = "info", alt = { "note" } },
+            TODO = { icon = " ", color = "info", alt = { "todo" } },
+
+            REVIEW = { icon = " ", color = "hint", alt = { "review" } },
+            PERF = { icon = "", color = "hint", alt = { "perf" } },
+            TEST = { icon = "", color = "hint", alt = { "test" } },
         },
         colors = {
-            -- error = { "DiagnosticError", "ErrorMsg", "#DC2626" },
-            -- warning = { "DiagnosticWarn", "WarningMsg", "#FBBF24" },
-            -- info = { "DiagnosticInfo", "#2563EB" },
-            -- hint = { "DiagnosticHint", "#10B981" },
-            -- default = { "Identifier", "#7C3AED" },
-            -- test = { "Identifier", "#FF00FF" }
-
-            fix = { "DiagnosticError", "#f38ba8" },
-            warn = { "DiagnosticWarn", "#fab387" },
-            note = { "DiagnosticHint", "#b4befe" },
-            todo = { "DiagnosticInfo", "#f9e2af" },
-
-            impl = { "Keyword", "#cba6f7" },
-            refactor = { "Function", "#94e2d5" },
-            review = { "Special", "#f9e2af" },
-            test = { "Identifier", "#f5c2e7" },
-            perf = { "String", "#a6e3a1" },
+            error = { "DiagnosticError", "ErrorMsg", "#DC2626" },
+            warning = { "DiagnosticWarn", "WarningMsg", "#FBBF24" },
+            info = { "DiagnosticInfo", "#2563EB" },
+            hint = { "DiagnosticHint", "#10B981" },
+            default = { "Identifier", "#7C3AED" },
+            test = { "Identifier", "#FF00FF" },
         },
     },
 }
